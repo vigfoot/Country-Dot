85dd3c8a33baaab969752bd1d1f7bee59020c177
vigfoot <48287365+vigfoot@users.noreply.github.com>
2023-03-04 19:32:30 +0900

    initialize

---
+++ src/main/java/com/vigfoot/countrydot/CountryDotApplication.java
@@ -0,0 +1,13 @@
+package com.vigfoot.countrydot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryDotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountryDotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vigfoot/countrydot/CountryDotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vigfoot.countrydot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryDotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
