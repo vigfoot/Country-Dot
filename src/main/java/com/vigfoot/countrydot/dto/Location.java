93f0b0ed29a6c14135afe0e04247212bac9a527d
Xasquatch <bigfoot7774@gmail.com>
2023-03-09 17:34:33 +0900

    폴리곤 데이터 바인딩위한 준비

--- src/main/java/com/vigfoot/countrydot/dto/Location.java
+++ src/main/java/com/vigfoot/countrydot/dto/Location.java
@@ -2,12 +2,37 @@ package com.vigfoot.countrydot.dto;
 
 import lombok.Getter;
 
+import java.util.List;
+
 public class Location {
 
     @Getter
-    public static class Coordinate {
-        private double latitude;
-        private double longitude;
+    public static class Polygon {
+        private String type;
+        private String name;
+        private List<String> crs;
+        private List<String> features;
+
     }
 
+    @Getter
+    public static class Feature {
+        private String type;
+        private Properties properties;
+        private Geometry geometry;
+    }
+
+    @Getter
+    public static class Properties {
+        private String GID_0;
+        private String COUNTRY;
+
+    }
+
+    @Getter
+    public static class Geometry {
+        private String type;
+        private List<Double[]> coordinates;
+
+    }
 }
--- src/main/java/com/vigfoot/countrydot/vo/Geographic.java
+++ src/main/java/com/vigfoot/countrydot/vo/Geographic.java
@@ -3,6 +3,10 @@ package com.vigfoot.countrydot.vo;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
+import lombok.Setter;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class Geographic {
 
@@ -11,12 +15,37 @@ public class Geographic {
     public static class Coordinate {
         private double latitude;
         private double longitude;
+        @Setter
+        private boolean isLand;
     }
 
+    @Getter
     @Builder
-    public static class Place {
-        private Coordinate coordinate;
-        private boolean isLand;
+    public static class Land {
+        private List<Coordinate> coordinates;
+
+        public void addCoordinate(Coordinate coordinate){
+            if (coordinates == null)
+                coordinates = new LinkedList<>();
+
+            coordinates.add(coordinate);
+        }
+    }
+
+    @Data
+    @Builder
+    public static class Country {
+        private Continent continent;
+        private String code;
+        private String name;
+        private List<Land> lands;
+
+        public void addLand(Land land){
+            if (lands == null)
+                lands = new LinkedList<>();
+
+            lands.add(land);
+        }
     }
 
     @Builder
@@ -24,11 +53,5 @@ public class Geographic {
         private String code;
         private String name;
 
-    }
-    @Data
-    public static class Country {
-        private Continent continent;
-        private String code;
-        private String name;
     }
 }
